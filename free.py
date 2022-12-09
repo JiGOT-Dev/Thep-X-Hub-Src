a37775d7adf731e26c76dcd630ef0cb8fa631c06
Meoaw メ <118798292+JiGOT-Dev@users.noreply.github.com>
2022-12-09 09:31:14 +0700

    Update free.py

--- free.py
+++ free.py
@@ -1,8 +1,8 @@
 free_config = {
-    "token":"MTA0OTg2MjE2NzIxMTU2MTEzMg.GPPJdN.nTfnGsEi3CXURHrGoC9gqZILoWmgqAcujXT4TE",
-    "panel_ch":"1050227947451195433",
-    "free_ch":"1050222302832365619",
-    "log_ch":"1050238392971046982",
+    "token":"",
+    "panel_ch":"",
+    "free_ch":"",
+    "log_ch":"",
     "send_panel":"1",
     "auto_teach":"1",
     "new_queue":"1"
